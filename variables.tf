304d903b8b98ef9bcc9e9dddf4dcb7d3df33d783
Joseth Fernandes <47750769+josethf93@users.noreply.github.com>
2021-04-09 09:39:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,8 @@ variable "vpc_id" {
 
 variable "amiid" {
   description = "AMI ID of the instance"
-  default     = "ami-0742b4e673072066f"
+  #default     = "ami-0742b4e673072066f"
+  default     = "ami-0fde50fcbcd46f2f7"   #SUSE Linux Enterprise Server 15 Service Pack 2 (HVM), EBS General Purpose (SSD) Volume Type. Amazon EC2 AMI Tools preinstalled; Apache 2.2, MySQL 5.5, PHP 5.3, and Ruby 1.8.7 available.
   type        = string
 }
 
